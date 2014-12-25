8ad308ad4e9f259f5321e6bd052593b7f37f056b
hoadh <danghuyhoa@gmail.com>
2014-12-25 08:11:25 +0700

    get post from request

--- routes/register.js
+++ routes/register.js
@@ -4,4 +4,10 @@ router.get('/', function(req,res) {
 	res.render('register', {title : 'Register'});
 });
 
+router.post('/', function(req,res) {
+	var body = req.body;
+	res.send('Registered Information: '+body.email);
+	console.log(req.body);
+});
+
 module.exports = router;
